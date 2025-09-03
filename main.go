42625d3f5f7ef09900407c4e6c18e011d8610a03
Debjoti Chandra <debjotic@gmail.com>
2025-09-03 22:35:38 +0530

    Initial Files

--- main.go
+++ main.go
@@ -6,10 +6,11 @@ func main() {
 
 	result := make(map[float64][]float64)
 
-	for index, taxRate := range taxRates {
-		var taxIncludedP
+	for _, taxRate := range taxRates {
+		taxIncludedPrices := make([]float64, len(prices))
 		for priceIndex, price := range prices{
-
+			taxIncludedPrices[priceIndex] = price * (1+taxRate)
 		}
+		result[]
 	}
 }
