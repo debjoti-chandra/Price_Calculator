fee1207bd515c931c9d5e3c94373b9f492a80554
Debjoti Chandra <debjotic@gmail.com>
2025-09-03 17:10:38 +0530

    Initial Files

---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+func main() {
+	prices := []float64{10,20,30}
+	taxRates := []float64{0,0.07,0.1,0.15}
+
+	result := make(map[float64][]float64)
+
+	for index, taxRate := range taxRates {
+		var taxIncludedP
+		for priceIndex, price := range prices{
+
+		}
+	}
+}
